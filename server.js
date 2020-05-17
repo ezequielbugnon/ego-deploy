3474dd4c5711bc039d37f4f0d892cf44273d32db
ezequiel <bugnonezequiel@gmail.com>
2020-05-17 19:55:30 -0300

    first commit 4

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + 'dist/ego-challege'));
+app.use(express.static(__dirname + '/dist/ego-challege'));
 
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname+'dist/ego-challege/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/ego-challege/index.html'));
 });
 
 app.listen( process.env.PORT || 8080);
